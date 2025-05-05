c604359ec88f94b2e586b2ccbc4253e7758f0e99
holyprof <Tobbest17@gmail.com>
2025-05-05 17:03:42 +0100

    Add header.php

---
+++ user/header.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en-US" class="js">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="author" content="PHENOMNET">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="PHENOMNET - Trusted Trade Partner">
+    <meta name="keywords" content="Escrow, PHENOMNET,p2p" />
+
+    <!-- Fav Icon -->
+    <link rel="shortcut icon" type="image/x-icon" href="./dist/img/p2pdarkicon.png" />
+    <link rel="icon" type="image/x-icon" href="./dist/img/p2pdarkicon.png" />
+
+    <!-- Site Title  -->
+    <title>Dashboard - PHENOMNET</title>
+
+    <!-- STYLESHEETS -->
+    <link rel="stylesheet" href="./assets/css/custom.css">
+    <link rel="stylesheet" href="./assets/css/vendor.bundle49f7.css">
+    <link rel="stylesheet" href="./assets/css/style.css" id="layoutstyle">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- <link rel="stylesheet" type="text/css" href="./dist/bootstrap/css/bootstrap.min.css"> -->
+    <!--Toastr-->
+<link rel="stylesheet" type="text/css" href="dist/css/toastr.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <!-- font awesome -->
+    <script src="https://kit.fontawesome.com/b1e2cc998d.js" crossorigin="anonymous"></script>
+    <style type="text/css">
+        .card-token{background-color:#5867dd !important; 
+            background-image:linear-gradient(45deg,#581845 80%, #FFAB00 20%) !important;}
+        .topbar,.topbar-wrap,.is-sticky,.topbar.is-sticky{background-color:#FFAB00 !important; 
+            background-image:linear-gradient(45deg,#581845 80%, #FFAB00 20%) !important;
+            color:#fff !important;}
+            /*.token-info-sub,.card-title{color:#FFAB00 !important; }*/
+          /*.toggle-tigger.user-thumb{background-color: transparent !important;}
+*/
+           .navbar-menu > li > a{color:#FFAB00 !important;}
+            .simplebar-scroll-content {margin-bottom: 0px !important;}
+        .chat-contacts-title{padding-left: 10px;}
+        a.drop-toggle::after{content:none !important;}
+        .p2pBtn{background-color:#FFAB00 !important; border:1px solid #5867dd !important; color:#fff !important;}
+        .p2pText{color:#FFAB00 !important;}
+        .p2pBorder{border:1px solid #FFAB00 !important;}
+       .dataTables_info{display: none !important;}
+
+       /*Tooltip on view-addresses page*/
+           .tooltip {
+      position: relative;
+      display: inline-block;
+    }
+
+    .tooltip .tooltiptext {
+      visibility: hidden;
+      width: 140px;
+      background-color: #555;
+      color: #fff;
+      text-align: center;
+      border-radius: 6px;
+      padding: 5px;
+      position: absolute;
+      z-index: 1;
+      bottom: 150%;
+      left: 50%;
+      margin-left: -75px;
+      opacity: 0;
+      transition: opacity 0.3s;
+    }
+
+    .tooltip .tooltiptext::after {
+      content: "";
+      position: absolute;
+      top: 100%;
+      left: 50%;
+      margin-left: -5px;
+      border-width: 5px;
+      border-style: solid;
+      border-color: #555 transparent transparent transparent;
+    }
+
+    .tooltip:hover .tooltiptext {
+      visibility: visible;
+      opacity: 1;
+    }
+
+    .logoTxt{font-weight:900; font-family:tahoma,poppins,open-sans; 
+        font-size:24px; color:#fff;}
+
+    .logoHr{width:50%; border:1px solid #fff !important; position:relative; margin-top: 0; padding-top: 0;}
+ @media screen and (min-width:601px){
+.showTb{display: none;}
+}
+    @media screen and (max-width: 600px){
+
+  tr,td,th,.btnTarget{
+    width:100% !important;
+    float:left !important;
+  }
+
+  .hideTb{display: none !important;}
+  /*.showTb{display: grid !important; grid-template-columns: auto auto !important;}*/
+
+  .myrow{width: 100% !important;}
+  .mycolumn{width: 50% !important; float: left !important;}
+
+  .intermediate{font-family: tahoma; font-weight:400;}
+  .intermediate:nth-child(odd){background-color: lightgrey;}
+
+}
+li > a:hover, .orange{color: #FFAB00 !important;}
+    .bgOrange{background:linear-gradient(to right,#FFAB00,orange,white) !important;}
+    hr{color:#FFAB00;}
+.cursive{font-family:cursive !important;}
+    </style>
+</head>
